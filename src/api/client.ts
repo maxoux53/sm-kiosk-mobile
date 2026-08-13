8c7a0ab96e9803bf68d6ee0cbd3b6cc3b932dd64
Max <71935156+maxoux53@users.noreply.github.com>
2026-08-13 18:27:36 +0200

    feat: setup secure store and persist jwt

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,8 +1,18 @@
 import axios from "axios";
+import { token } from "./secureStore";
 
-const API_BASE_URL = "https://sm-kiosk-production.up.railway.app/v1/";
-export const API_INTERACT_ROUTE = "interact/";
+const API_BASE_URL: string = "https://sm-kiosk-production.up.railway.app/v1/";
+export const API_INTERACT_ROUTE: string = "interact/";
 
-export const apiClient = axios.create({
-    baseURL: API_BASE_URL,
-});
+export const apiClient = axios
+    .create({
+        baseURL: API_BASE_URL,
+    })
+    .interceptors.request.use(async (config) => {
+        const jwt = await token.read();
+        if (jwt) {
+            config.headers.Authorization = `Bearer ${jwt}`;
+        }
+        return config;
+    })
+;
---
+++ src/api/secureStore.ts
@@ -0,0 +1,15 @@
+import * as SecureStore from 'expo-secure-store';
+
+const SM_JWT_KEY: string = "token";
+
+export const token = {
+    write: async (jwt: string): Promise<void> => {
+        await SecureStore.setItemAsync(SM_JWT_KEY, jwt);
+    },
+    read: async (): Promise<string | null> => {
+        return await SecureStore.getItemAsync(SM_JWT_KEY);
+    },
+    clear: async (): Promise<void> => {
+        await SecureStore.deleteItemAsync(SM_JWT_KEY);
+    }
+}
