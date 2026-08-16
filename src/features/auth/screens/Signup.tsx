38625a2380da787254ec0a89268e4935305ac7af
Zencaw <161458851+zencaaw@users.noreply.github.com>
2026-08-16 17:01:24 +0200

    fix : mediatypes

--- src/features/auth/screens/Signup.tsx
+++ src/features/auth/screens/Signup.tsx
@@ -33,7 +33,7 @@ export default function Signup() {
       }
 
       const result = await ImagePicker.launchImageLibraryAsync({
-        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        mediaTypes: ['images'],
         allowsEditing: true,
         aspect: [1, 1],
         quality: 0.8,
